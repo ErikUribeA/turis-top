236c51c4a8328a72b7072eda16ca87bb786dda17
Erik <erickjosueuribe@gmail.com>
2025-01-28 11:13:33 -0500

    changes done

--- src/components/buttons/TurisBotMain.tsx
+++ src/components/buttons/TurisBotMain.tsx
@@ -1,34 +1,33 @@
-"use client";
+"use client"
 
-import { useState } from "react";
-import Image from "next/image";
-import { motion } from "framer-motion";
-import { useSession, signIn, signOut } from "next-auth/react";
-import { useTranslations } from "next-intl";
+import { useState } from "react"
+import Image from "next/image"
+import { motion } from "framer-motion"
+import { useSession, signIn, signOut } from "next-auth/react"
+import { useTheme } from "@/context/ThemeContext"
+
 
 interface InteractiveImageProps {
-  imageUrl: string;
-  welcomeMessage: string;
+  imageUrl: string
+  welcomeMessage: string
 }
 
 export default function TurisBotMain({ imageUrl, welcomeMessage }: InteractiveImageProps) {
-  const [isHovered, setIsHovered] = useState(false);
-  const { data: session } = useSession();
-  const t = useTranslations();
+  const [isHovered, setIsHovered] = useState(false)
+  const { data: session } = useSession()
+  const { theme } = useTheme() // Use the theme from your ThemeContext
 
   const handleClick = async () => {
     if (session && session.user) {
-      // Usuario autenticado: cerrar sesión
-      await signOut({ callbackUrl: "/" });
+      await signOut({ callbackUrl: "/" })
     } else {
-      // Usuario no autenticado: iniciar sesión
-      await signIn("google");
+      await signIn("google")
     }
-  };
+  }
 
   return (
     <div
-      className="fixed right-4 bottom-4 z-50"
+      className={`fixed right-2 bottom-2 sm:right-4 sm:bottom-4 z-50 ${theme === "dark" ? "dark" : ""}`}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
@@ -44,6 +43,7 @@ export default function TurisBotMain({ imageUrl, welcomeMessage }: InteractiveIm
           alt="Interactive Image"
           width={300}
           height={100}
+          className="w-44 h-auto sm:w-50 md:w-64 lg:w-70 xl:w-75"
         />
       </motion.div>
 
@@ -53,12 +53,13 @@ export default function TurisBotMain({ imageUrl, welcomeMessage }: InteractiveIm
           animate={{ opacity: 1, scale: 1 }}
           exit={{ opacity: 0, scale: 0.8 }}
           transition={{ duration: 0.3 }}
-          className="absolute bottom-full left-0 mb-2 p-4 bg-white rounded-lg shadow-lg max-w-xs dark:bg-gray-800 "
+          className="absolute bottom-full left-0 mb-2 p-4 bg-white dark:bg-gray-800 rounded-lg shadow-lg max-w-xs"
         >
-          <div className="dark:text-gray-200 text-gray-800">{welcomeMessage}</div>
-          <div className="absolute left-4 bottom-0 w-4 h-4 bg-white dark:bg-gray-600 transform rotate-45 translate-y-2" />
+          <div className="text-gray-800 dark:text-gray-200 text-sm sm:text-base">{welcomeMessage}</div>
+          <div className="absolute left-4 bottom-0 w-4 h-4 bg-white dark:bg-gray-800 transform rotate-45 translate-y-2" />
         </motion.div>
       )}
     </div>
-  );
+  )
 }
+
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,54 +6,67 @@ import { useTheme } from '../../context/ThemeContext';
 import SelectLanguage from '../inputs/SelectLanguage';
 import { LoginButton } from '../buttons/LoginButton';
 import styles from './Header.module.scss';
-import {useTranslations} from 'next-intl';
+import { useTranslations } from 'next-intl';
+import { PiSunDimFill } from "react-icons/pi";
+import { FaMoon } from "react-icons/fa";
+
 
 export default function Header() {
-    const [menuOpen, setMenuOpen] = useState(false);
-    const { theme, toggleTheme } = useTheme();
-    const t = useTranslations('header');
+  const [menuOpen, setMenuOpen] = useState(false);
+  const { theme, toggleTheme } = useTheme();
+  const t = useTranslations('header');
 
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
-    };
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
-    return (
-        <header className={`${styles.header} ${theme === 'dark' ? styles.dark : styles.light}`}>
-          <div className={styles.headerContainer}>
-            <Link href="/" className={styles.logo}>
-              TurisTop
-            </Link>
-            <button className={styles.hamburger} onClick={toggleMenu} aria-label="Toggle Menu">
-              <span className={menuOpen ? styles.active : ''}></span>
-            </button>
-            <nav className={`${styles.nav} ${theme === 'dark' ? styles.dark : styles.light} ${menuOpen ? styles.open : ''}`}>
-              <Link href="#destinations" className={styles.navLink}>
-                {t('destinations')}
-              </Link>
-              <Link href="#about" className={styles.navLink}>
-                {t('about')}
-              </Link>
-              <Link href="#contact" className={styles.navLink}>
-                {t('contact')}
-              </Link>
-              <div className={styles.mobileActions}>
-                <SelectLanguage />
-                <LoginButton />
-              </div>
-            </nav>
-            <div className={styles.desktopActions}>
+  return (
+    <header className={`${styles.header} ${theme === 'dark' ? styles.dark : styles.light}`}>
+      <div className={styles.headerContainer}>
+        <Link href="/" className={styles.logo}>
+          TurisTop
+        </Link>
+        <div>
+          <button className={styles.hamburger} onClick={toggleMenu} aria-label="Toggle Menu">
+            <span className={`${styles.hamburgerIcon} ${menuOpen ? styles.active : ''}`}></span>
+          </button>
+        </div>
+        <nav className={`${styles.nav} ${theme === 'dark' ? styles.dark : styles.light} ${menuOpen ? styles.open : ''}`}>
+          <Link href="#destinations" className={styles.navLink}>
+            {t('destinations')}
+          </Link>
+          <Link href="#about" className={styles.navLink}>
+            {t('about')}
+          </Link>
+          <Link href="#contact" className={styles.navLink}>
+            {t('contact')}
+          </Link>
+          <div className={styles.mobileActions}>
+            <div className={styles.mobileLogin}>
               <SelectLanguage />
               <LoginButton />
-              {/* Botón de cambio de tema */}
-              <button onClick={toggleTheme} aria-label="Toggle Theme" className={styles.themeToggle}>
-                {theme === 'light' ? (
-                  <span role="img" aria-label="Sun">🌞</span>
-                ) : (
-                  <span role="img" aria-label="Moon">🌙</span>
-                )}
-              </button>
             </div>
+            <button onClick={toggleTheme} aria-label="Toggle Theme" className={styles.themeToggle}>
+              {theme === 'light' ? (
+                <span role="img" aria-label="Moon"> <FaMoon className='text-[1em]'/> </span>
+              ) : (
+                <span role="img" aria-label="Sun"> <PiSunDimFill  className='text-[1.5em]'/> </span>
+              )}
+            </button>
           </div>
-        </header>
-      );
-    }
+        </nav>
+        <div className={styles.desktopActions}>
+          <SelectLanguage />
+          <LoginButton />
+          <button onClick={toggleTheme} aria-label="Toggle Theme" className={styles.themeToggle}>
+            {theme === 'light' ? (
+              <span role="img" aria-label="Moon"> <FaMoon /> </span>
+            ) : (
+              <span role="img" aria-label="Sun"> <PiSunDimFill /></span>
+            )}
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+}
